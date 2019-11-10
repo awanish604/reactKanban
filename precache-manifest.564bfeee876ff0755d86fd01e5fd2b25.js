387d4a3596abf7901b5ba22b88eae9d6331e501f
awanish <awanish.604@gmail.com>
2019-11-10 18:36:21 +0530

    Updates

---
+++ precache-manifest.564bfeee876ff0755d86fd01e5fd2b25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9030e3dab056158ce9ef7fca570c979c",
+    "url": "/reactKanban/index.html"
+  },
+  {
+    "revision": "3970d0cfb3a1a174ec3b",
+    "url": "/reactKanban/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "3970d0cfb3a1a174ec3b",
+    "url": "/reactKanban/static/js/2.f70a3ade.chunk.js"
+  },
+  {
+    "revision": "438705d4e8aa04ab7b18",
+    "url": "/reactKanban/static/js/main.10b098c9.chunk.js"
+  },
+  {
+    "revision": "799b23455a4509d4201d",
+    "url": "/reactKanban/static/js/runtime-main.9ac525f5.js"
+  }
+]);
